7d9ebb7baa873849b982d80dac159f0a327cd76b
Denny Shin <hyunshin96@gmail.com>
2020-11-26 15:18:49 +1100

    created a simple maze. player moves to destination avoiding the walls. no controls added yet

---
+++ Maze/Assets/Scripts/PlayerMove.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using UnityEngine.AI;
+
+public class PlayerMove : MonoBehaviour
+{
+    [SerializeField]
+    Transform _destination;
+
+    private NavMeshAgent _navMeshAgent;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        _navMeshAgent = GetComponent<NavMeshAgent>();
+    }
+
+    void Update()
+    {
+        SetDestination();
+    }
+
+    private void SetDestination()
+    {
+        Vector3 targetVector = _destination.transform.position;
+        _navMeshAgent.SetDestination(targetVector);
+        print(_destination.transform);
+    }
+}
